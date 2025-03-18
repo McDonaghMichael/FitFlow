f181cb545804bb2848fe1b7170ed3d5e81817437
McDonaghMichael <G00421944@atu.ie>
2025-03-18 12:47:36 +0000

    Added a terms and conditions page

--- app/src/app/pages/authentication/terms/terms.page.ts
+++ app/src/app/pages/authentication/terms/terms.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {IonCol, IonContent, IonGrid, IonHeader, IonRow, IonTitle, IonToolbar} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-terms',
   templateUrl: './terms.page.html',
   styleUrls: ['./terms.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCol, IonGrid, IonRow]
 })
 export class TermsPage implements OnInit {
 
