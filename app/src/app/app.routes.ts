e29456a3ee769ec821a11a7e4144868c5753b818
McDonaghMichael <G00421944@atu.ie>
2025-03-18 11:46:33 +0000

    Updated routes for the app

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -2,76 +2,67 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    loadComponent: () => import('./pages/main/homepage/homepage.page').then(m => m.HomepagePage)
   },
   {
-    path: 'landing',
+    path: 'authentication/landing',
     loadComponent: () => import('./pages/authentication/landing/landing.page').then(m => m.LandingPage)
   },
   {
-    path: 'register',
+    path: 'authentication/register',
     loadComponent: () => import('./pages/authentication/register/register.page').then(m => m.RegisterPage)
   },
   {
-    path: 'terms',
+    path: 'authentication/terms',
     loadComponent: () => import('./pages/authentication/terms/terms.page').then(m => m.TermsPage)
   },
   {
-    path: 'forgot-password',
+    path: 'authentication/forgot-password',
     loadComponent: () => import('./pages/authentication/forgot-password/forgot-password.page').then(m => m.ForgotPasswordPage)
-  },
-  {
-    path: 'homepage',
-    loadComponent: () => import('./pages/main/homepage/homepage.page').then(m => m.HomepagePage)
   },
   {
     path: 'settings',
     loadComponent: () => import('./pages/main/settings/settings.page').then(m => m.SettingsPage)
   },
   {
-    path: 'fitness-data',
+    path: 'settings/fitness-data',
     loadComponent: () => import('./pages/main/settings/types/fitness-data/fitness-data.page').then(m => m.FitnessDataPage)
   },
   {
-    path: 'account-management',
+    path: 'settings/account-management',
     loadComponent: () => import('./pages/main/settings/types/account-management/account-management.page').then(m => m.AccountManagementPage)
   },
   {
-    path: 'appearance',
+    path: 'settings/appearance',
     loadComponent: () => import('./pages/main/settings/types/appearance/appearance.page').then(m => m.AppearancePage)
   },
   {
-    path: 'goals',
+    path: 'settings/goals',
     loadComponent: () => import('./pages/main/settings/types/goals/goals.page').then(m => m.GoalsPage)
   },
   {
-    path: 'notifications',
+    path: 'settings/notifications',
     loadComponent: () => import('./pages/main/settings/types/notifications/notifications.page').then(m => m.NotificationsPage)
   },
   {
-    path: 'report-issue',
+    path: 'settings/report-issue',
     loadComponent: () => import('./pages/main/settings/types/report-issue/report-issue.page').then(m => m.ReportIssuePage)
   },
   {
-    path: 'barcode-scanner',
+    path: 'product/barcode-scanner',
     loadComponent: () => import('./pages/features/products/barcode-scanner/barcode-scanner.page').then(m => m.BarcodeScannerPage)
   },
   {
-    path: 'enter-manually',
+    path: 'product/enter-manually',
     loadComponent: () => import('./pages/features/products/enter-manually/enter-manually.page').then(m => m.EnterManuallyPage)
   },
   {
-    path: 'product-information',
+    path: 'product/product-information',
     loadComponent: () => import('./pages/features/products/product-information/product-information.page').then(m => m.ProductInformationPage)
   },
   {
-    path: 'reviews',
+    path: 'product/reviews',
     loadComponent: () => import('./pages/features/products/reviews/reviews.page').then(m => m.ReviewsPage)
   },
 ];
