750195808d835e89f947b9023efcf4ac492e3ae4
McDonaghMichael <G00421944@atu.ie>
2025-03-18 12:18:42 +0000

    Added form content to register page

--- app/src/app/pages/authentication/register/register.page.ts
+++ app/src/app/pages/authentication/register/register.page.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonButton, IonCheckbox,
+  IonCol,
+  IonContent, IonGrid,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonList, IonRow,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonList, IonItem, IonInput, IonCol, IonGrid, IonRow, IonButton, IonCheckbox]
 })
 export class RegisterPage implements OnInit {
 
