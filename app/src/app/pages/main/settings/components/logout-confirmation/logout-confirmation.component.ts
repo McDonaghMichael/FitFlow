7a2a94098058213d3a3a5aca1ba38c184b6bb20a
McDonaghMichael <G00421944@atu.ie>
2025-03-31 17:54:29 +0100

    Updated all pages and created the logout modal

--- app/src/app/pages/main/settings/components/logout-confirmation/logout-confirmation.component.ts
+++ app/src/app/pages/main/settings/components/logout-confirmation/logout-confirmation.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle
+} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-logout-confirmation',
   templateUrl: './logout-confirmation.component.html',
   styleUrls: ['./logout-confirmation.component.scss'],
+  imports: [
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardContent,
+    IonButton
+  ]
 })
 export class LogoutConfirmationComponent  implements OnInit {
 
+  @Input() onCancel!: () => void;
   constructor() { }
 
   ngOnInit() {}
--- app/src/app/pages/main/settings/settings.page.ts
+++ app/src/app/pages/main/settings/settings.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
@@ -14,15 +14,22 @@ import {
 import {TabMenuComponent} from "../../../components/tab-menu/tab-menu.component";
 import {AccountManagementPage} from "./types/account-management/account-management.page";
 import {RouterLink, RouterModule} from "@angular/router";
+import {LogoutConfirmationComponent} from "./components/logout-confirmation/logout-confirmation.component";
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [IonContent, RouterModule, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItem, IonLabel, TabMenuComponent, IonList, IonNav, RouterLink]
+  imports: [IonContent, RouterModule, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItem, IonLabel, TabMenuComponent, IonList, IonNav, RouterLink, LogoutConfirmationComponent]
 })
 export class SettingsPage implements OnInit {
+
+  @Input() logoutConfirmationShow = false;
+
+  toggleLogoutConfirmationModal() {
+    this.logoutConfirmationShow = !this.logoutConfirmationShow;
+  }
 
   constructor() { }
 
