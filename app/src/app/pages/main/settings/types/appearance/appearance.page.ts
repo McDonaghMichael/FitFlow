a3ee1720f99944370c9f00ca4d1a3dcff7cf6e5e
McDonaghMichael <G00421944@atu.ie>
2025-04-01 10:43:52 +0100

    Added light and dark options to appearance.page.html

--- app/src/app/pages/main/settings/types/appearance/appearance.page.ts
+++ app/src/app/pages/main/settings/types/appearance/appearance.page.ts
@@ -1,20 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {IonCol, IonContent, IonGrid, IonHeader, IonRow, IonTitle, IonToolbar} from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader, IonItem, IonLabel,
+  IonRow,
+  IonTitle, IonToggle,
+  IonToolbar
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-appearance',
   templateUrl: './appearance.page.html',
   styleUrls: ['./appearance.page.scss'],
   standalone: true,
-    imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCol, IonGrid, IonRow]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCol, IonGrid, IonRow, IonCard, IonCardContent, IonItem, IonLabel, IonToggle, IonButton]
 })
 export class AppearancePage implements OnInit {
+
+  isDarkMode = false;
 
   constructor() { }
 
   ngOnInit() {
+    const savedTheme = localStorage.getItem('darkMode');
+    if (savedTheme) {
+      this.isDarkMode = savedTheme === 'true';
+    }
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('darkMode', this.isDarkMode.toString());
   }
 
 }
