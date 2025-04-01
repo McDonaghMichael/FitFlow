44de9531acd2984341cca4a342f5c5132709ad74
McDonaghMichael <G00421944@atu.ie>
2025-04-01 12:06:15 +0100

    Added error checking to reporting

--- app/src/app/pages/main/settings/types/report-issue/report-issue.page.ts
+++ app/src/app/pages/main/settings/types/report-issue/report-issue.page.ts
@@ -14,17 +14,22 @@ import {
 } from '@ionic/angular/standalone';
 import {SettingsTabMenuComponent} from "../../components/settings-tab-menu/settings-tab-menu.component";
 import {NotificationComponent} from "../../../../../components/notification/notification.component";
+import {ErrorAlertComponent} from "../../../../../components/error-alert/error-alert.component";
 
 @Component({
   selector: 'app-report-issue',
   templateUrl: './report-issue.page.html',
   styleUrls: ['./report-issue.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardContent, IonCol, IonGrid, IonItem, IonLabel, IonRow, IonToggle, IonButton, IonInput, IonTextarea, SettingsTabMenuComponent, NotificationComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardContent, IonCol, IonGrid, IonItem, IonLabel, IonRow, IonToggle, IonButton, IonInput, IonTextarea, SettingsTabMenuComponent, NotificationComponent, ErrorAlertComponent]
 })
 export class ReportIssuePage implements OnInit {
 
   issueReported: boolean = false;
+  issue: string = "";
+
+  error: boolean = false;
+  errorMessage: string = "An error has occurred!";
 
   constructor() { }
 
@@ -32,10 +37,30 @@ export class ReportIssuePage implements OnInit {
   }
 
   submitReport() {
+    if(!this.runValidation()) return;
+
     this.issueReported = true;
     setTimeout(() => {
       this.issueReported = false;
     }, 3000);
+  }
+
+  runValidation() : boolean {
+    this.error = false;
+    this.errorMessage = "An error has occurred";
+
+    setTimeout(() => {
+      this.error = false;
+    }, 3000);
+
+    console.log(this.issue.length)
+    if(this.issue.length <= 0) {
+      this.error = true;
+      this.errorMessage = "Issue cannot be empty";
+      return false;
+    }
+
+    return true;
   }
 
 }
