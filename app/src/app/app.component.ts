b797803231bf601c8cdc1d3aa526517545ff2a51
McDonaghMichael <G00421944@atu.ie>
2025-04-01 10:36:39 +0100

    Updated Account page

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
   standalone: true,
   imports: [IonApp, IonRouterOutlet],
 })
--- app/src/app/pages/main/settings/types/account-management/account-management.page.ts
+++ app/src/app/pages/main/settings/types/account-management/account-management.page.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonCheckbox,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader, IonInput, IonItem, IonList, IonRow,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-account-management',
   templateUrl: './account-management.page.html',
   styleUrls: ['./account-management.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonCheckbox, IonCol, IonGrid, IonInput, IonItem, IonList, IonRow, RouterLink]
 })
 export class AccountManagementPage implements OnInit {
 
