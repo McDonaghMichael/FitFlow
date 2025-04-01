1a71293b7ac69b251d0b29fac2ec28171c15e0da
McDonaghMichael <G00421944@atu.ie>
2025-04-01 10:37:01 +0100

    Updated goals.page.html

--- app/src/app/pages/main/settings/types/goals/goals.page.ts
+++ app/src/app/pages/main/settings/types/goals/goals.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {IonCol, IonContent, IonGrid, IonHeader, IonRow, IonTitle, IonToolbar} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-goals',
   templateUrl: './goals.page.html',
   styleUrls: ['./goals.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+    imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCol, IonGrid, IonRow]
 })
 export class GoalsPage implements OnInit {
 
