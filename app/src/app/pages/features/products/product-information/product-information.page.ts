08d2cc7c1a89b9a49549771e314f1ee27651f50a
McDonaghMichael <G00421944@atu.ie>
2025-04-01 14:16:05 +0100

    Added a chart that displays the product data

--- app/src/app/pages/features/products/product-information/product-information.page.ts
+++ app/src/app/pages/features/products/product-information/product-information.page.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {IonContent, IonHeader, IonIcon, IonTitle, IonToolbar} from '@ionic/angular/standalone';
 import {TabMenuComponent} from "../../../../components/tab-menu/tab-menu.component";
+import Chart from 'chart.js/auto';
+
 
 @Component({
   selector: 'app-product-information',
@@ -11,7 +13,7 @@ import {TabMenuComponent} from "../../../../components/tab-menu/tab-menu.compone
   standalone: true,
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonIcon, TabMenuComponent]
 })
-export class ProductInformationPage implements OnInit {
+export class ProductInformationPage implements OnInit, AfterViewInit {
 
   selectedProduct: number = 0;
 
@@ -23,9 +25,10 @@ export class ProductInformationPage implements OnInit {
   protein: number = 0;
   satisfaction: number = 0;
 
+
   foodArray = [
-    { id: "aaa", name: 'Apple', brand: 'Tesco', calories: 95, carbs: 25, protein: 0.5, fibre: 4, rating: 4.5, barcodeNumber: 1000 },
-    { id: "aba", name: 'Banana', brand: 'Lidl', calories: 105, carbs: 27, protein: 1.3, fibre: 3.1, rating: 4.7, barcodeNumber: 1400 },
+    { id: "aaa", name: 'Apple', brand: 'Tesco', calories: 95, carbs: 10, protein: 50, fibre: 40, rating: 4.5, barcodeNumber: 1000 },
+    { id: "aba", name: 'Banana', brand: 'Lidl', calories: 105, carbs: 27, protein: 10.3, fibre: 3.1, rating: 4.7, barcodeNumber: 1400 },
     { id: "aaea", name: 'Orange Juice', brand: 'Aldi', calories: 120, carbs: 30, protein: 2, fibre: 0.5, rating: 4.2, barcodeNumber: 143000 },
     { id: "aa43a", name: 'Granola Bar', brand: 'Dunnes', calories: 200, carbs: 30, protein: 4, fibre: 5, rating: 4.0, barcodeNumber: 1000 },
     { id: "agaa", name: 'Apple Pie', brand: 'Lidl', calories: 250, carbs: 35, protein: 3, fibre: 3, rating: 3.8, barcodeNumber: 100230 },
@@ -35,6 +38,42 @@ export class ProductInformationPage implements OnInit {
 
   ngOnInit() {
     this.updateProduct();
+  }
+
+  ngAfterViewInit() {
+    this.initChart();
+  }
+
+  initChart(): void {
+    const ctx = document.getElementById('myChart') as HTMLCanvasElement;
+    let data = {
+      labels: [],
+      datasets: [{
+        label: 'Product Information',
+        data: [this.carbs, this.fibre, this.protein],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(75, 192, 192)',
+          'rgb(255, 205, 86)'
+        ]
+      }]
+    };
+
+    new Chart(ctx, {
+      type: 'polarArea',
+      data: data,
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          tooltip: {
+            enabled: true,
+          }
+        }
+      }
+    });
   }
 
   updateProduct(): void {
