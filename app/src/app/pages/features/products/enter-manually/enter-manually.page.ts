85c3b19a73a15d6eaa0c5370604b5bd1b40cca50
McDonaghMichael <G00421944@atu.ie>
2025-04-01 13:28:37 +0100

    Added ability to search item by barcode number

--- app/src/app/pages/features/products/enter-manually/enter-manually.page.ts
+++ app/src/app/pages/features/products/enter-manually/enter-manually.page.ts
@@ -25,11 +25,11 @@ export class EnterManuallyPage implements OnInit {
   search: string = '';
 
   foodArray = [
-    { id: "aaa", name: 'Apple', brand: 'Tesco', calories: 95 },
-    { id: "aba", name: 'Banana', brand: 'Lidl', calories: 105 },
-    { id: "aaea", name: 'Orange Juice', brand: 'Aldi', calories: 120 },
-    { id: "aa43a", name: 'Granola Bar', brand: 'Dunnes', calories: 200 },
-    { id: "agaa", name: 'Apple Pie', brand: 'Lidl', calories: 250 },
+    { id: "aaa", name: 'Apple', brand: 'Tesco', calories: 95, barcodeNumber: 1000 },
+    { id: "aba", name: 'Banana', brand: 'Lidl', calories: 105, barcodeNumber: 1400 },
+    { id: "aaea", name: 'Orange Juice', brand: 'Aldi', calories: 120, barcodeNumber: 143000 },
+    { id: "aa43a", name: 'Granola Bar', brand: 'Dunnes', calories: 200, barcodeNumber: 1000 },
+    { id: "agaa", name: 'Apple Pie', brand: 'Lidl', calories: 250, barcodeNumber: 100230 },
   ];
 
   filteredFoodArray: any = [];
@@ -44,7 +44,8 @@ export class EnterManuallyPage implements OnInit {
     const value = event.target.value.toLowerCase();
     this.filteredFoodArray = this.foodArray.filter(food =>
       food.name.toLowerCase().includes(value) ||
-      food.brand.toLowerCase().includes(value)
+      food.brand.toLowerCase().includes(value) ||
+      food.barcodeNumber == value
     );
     console.log(this.filteredFoodArray);
   }
