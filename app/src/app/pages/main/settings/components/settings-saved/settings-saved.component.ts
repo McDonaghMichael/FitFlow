add10d20fc7d7c4854d73779f7b9ebd8c74c8195
McDonaghMichael <G00421944@atu.ie>
2025-04-01 11:18:48 +0100

    Created a settings saved component to be the notification of when settings get saved

---
+++ app/src/app/pages/main/settings/components/settings-saved/settings-saved.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-saved',
+  templateUrl: './settings-saved.component.html',
+  styleUrls: ['./settings-saved.component.scss'],
+})
+export class SettingsSavedComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
