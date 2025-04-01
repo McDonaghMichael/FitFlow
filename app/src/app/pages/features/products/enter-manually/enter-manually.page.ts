217843559b395258e23ba17a8a82ab1779960dda
McDonaghMichael <G00421944@atu.ie>
2025-04-01 13:16:05 +0100

    enter-manually.page.html now updates dynamically when searching food by name or brand

--- app/src/app/pages/features/products/enter-manually/enter-manually.page.ts
+++ app/src/app/pages/features/products/enter-manually/enter-manually.page.ts
@@ -1,21 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-import {TabMenuComponent} from "../../../../components/tab-menu/tab-menu.component";
+import {
+  IonCard, IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
+import { TabMenuComponent } from "../../../../components/tab-menu/tab-menu.component";
 
 @Component({
   selector: 'app-enter-manually',
   templateUrl: './enter-manually.page.html',
   styleUrls: ['./enter-manually.page.scss'],
   standalone: true,
-    imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TabMenuComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TabMenuComponent, IonCard, IonInput, IonCardHeader, IonCardTitle, IonCardContent]
 })
 export class EnterManuallyPage implements OnInit {
+
+  search: string = '';
+
+  foodArray = [
+    { id: "aaa", name: 'Apple', brand: 'Tesco', calories: 95 },
+    { id: "aba", name: 'Banana', brand: 'Lidl', calories: 105 },
+    { id: "aaea", name: 'Orange Juice', brand: 'Aldi', calories: 120 },
+    { id: "aa43a", name: 'Granola Bar', brand: 'Dunnes', calories: 200 },
+    { id: "agaa", name: 'Apple Pie', brand: 'Lidl', calories: 250 },
+  ];
+
+  filteredFoodArray: any = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.filteredFoodArray = [...this.foodArray];
   }
 
+  updateResults(event: any): void {
+    const value = event.target.value.toLowerCase();
+    this.filteredFoodArray = this.foodArray.filter(food =>
+      food.name.toLowerCase().includes(value) ||
+      food.brand.toLowerCase().includes(value)
+    );
+    console.log(this.filteredFoodArray);
+  }
 }
