faa861841c85208ed9d547af1b74787d12153a11
McDonaghMichael <G00421944@atu.ie>
2025-04-01 12:01:37 +0100

    Implemented error validation

--- app/src/app/pages/main/settings/types/account-management/account-management.page.ts
+++ app/src/app/pages/main/settings/types/account-management/account-management.page.ts
@@ -15,17 +15,20 @@ import {RouterLink} from "@angular/router";
 import {SettingsTabMenuComponent} from "../../components/settings-tab-menu/settings-tab-menu.component";
 import {LogoutConfirmationComponent} from "../../components/logout-confirmation/logout-confirmation.component";
 import {NotificationComponent} from "../../../../../components/notification/notification.component";
+import {ErrorAlertComponent} from "../../../../../components/error-alert/error-alert.component";
 
 @Component({
   selector: 'app-account-management',
   templateUrl: './account-management.page.html',
   styleUrls: ['./account-management.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule, IonCol, IonGrid, IonInput, IonItem, IonList, IonRow, SettingsTabMenuComponent, NotificationComponent]
+  imports: [IonContent, CommonModule, FormsModule, IonCol, IonGrid, IonInput, IonItem, IonList, IonRow, SettingsTabMenuComponent, NotificationComponent, ErrorAlertComponent]
 })
 export class AccountManagementPage implements OnInit {
 
   settingsSaved: boolean = false;
+  error: boolean = false;
+  errorMessage: string = "An error has occurred!";
 
   email: string = "";
   password: string = "";
@@ -38,12 +41,27 @@ export class AccountManagementPage implements OnInit {
   }
 
   saveSettings() : void {
+    if(!this.runValidation()) return;
+
     this.settingsSaved = true;
     setTimeout(() => {
       this.settingsSaved = false;
     }, 3000);
 
     this.saveData();
+  }
+
+  runValidation() : boolean {
+    this.error = false;
+    this.errorMessage = "An error has occurred";
+
+    if(this.newPassword !== this.confirmNewPassword) {
+      this.error = true;
+      this.errorMessage = "Passwords do not match";
+      return false;
+    }
+
+    return true;
   }
 
   saveData() : void {
