a9886dc05f95ddc39aa079588a66eaec62c24e5f
McDonaghMichael <G00421944@atu.ie>
2025-04-01 12:10:09 +0100

    Implemented error checking and data binding to fitness-data.page.html

--- app/src/app/pages/main/settings/types/fitness-data/fitness-data.page.ts
+++ app/src/app/pages/main/settings/types/fitness-data/fitness-data.page.ts
@@ -14,17 +14,25 @@ import {
 } from '@ionic/angular/standalone';
 import {SettingsTabMenuComponent} from "../../components/settings-tab-menu/settings-tab-menu.component";
 import {NotificationComponent} from "../../../../../components/notification/notification.component";
+import {ErrorAlertComponent} from "../../../../../components/error-alert/error-alert.component";
 
 @Component({
   selector: 'app-fitness-data',
   templateUrl: './fitness-data.page.html',
   styleUrls: ['./fitness-data.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCol, IonGrid, IonInput, IonItem, IonList, IonRow, IonCard, IonCardContent, IonLabel, IonNote, IonSelect, IonSelectOption, SettingsTabMenuComponent, NotificationComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCol, IonGrid, IonInput, IonItem, IonList, IonRow, IonCard, IonCardContent, IonLabel, IonNote, IonSelect, IonSelectOption, SettingsTabMenuComponent, NotificationComponent, ErrorAlertComponent]
 })
 export class FitnessDataPage implements OnInit {
 
   settingsSaved: boolean = false;
+
+  error: boolean = false;
+  errorMessage: string = "An error has occurred!";
+
+  height: number = 0;
+  weight: number = 0;
+  gender: string = "";
 
   constructor() { }
 
@@ -32,10 +40,43 @@ export class FitnessDataPage implements OnInit {
   }
 
   saveSettings() {
+    if(!this.runValidation()) return;
+
     this.settingsSaved = true;
     setTimeout(() => {
       this.settingsSaved = false;
     }, 3000);
+
+    this.saveData()
+  }
+
+  runValidation() : boolean {
+    this.error = false;
+    this.errorMessage = "An error has occurred";
+
+    setTimeout(() => {
+      this.error = false;
+    }, 3000);
+
+    if(this.gender == "") {
+      this.error = true;
+      this.errorMessage = "Please select a gender";
+      return false;
+    }
+
+    if(this.height <= 0 || this.weight <= 0) {
+      this.error = true;
+      this.errorMessage = "Height and/or Weight cannot be less than or equal to 0";
+      return false;
+    }
+
+    return true;
+  }
+
+  saveData() : void {
+    console.log(this.height);
+    console.log(this.weight);
+    console.log(this.gender);
   }
 
 }
