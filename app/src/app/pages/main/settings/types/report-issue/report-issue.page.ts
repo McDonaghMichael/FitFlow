edbc08ba5bfeb7232182921029f7372fd6f07993
McDonaghMichael <G00421944@atu.ie>
2025-04-01 10:57:02 +0100

    Added input box to report-issue.page.html

--- app/src/app/pages/main/settings/types/report-issue/report-issue.page.ts
+++ app/src/app/pages/main/settings/types/report-issue/report-issue.page.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader, IonInput, IonItem, IonLabel, IonRow,
+  IonTitle, IonToggle,
+  IonToolbar
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-report-issue',
   templateUrl: './report-issue.page.html',
   styleUrls: ['./report-issue.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardContent, IonCol, IonGrid, IonItem, IonLabel, IonRow, IonToggle, IonButton, IonInput]
 })
 export class ReportIssuePage implements OnInit {
 
