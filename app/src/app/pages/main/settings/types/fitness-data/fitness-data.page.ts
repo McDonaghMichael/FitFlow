39b09d03b8715a45c7e6331c788b3c0de0942c4b
McDonaghMichael <G00421944@atu.ie>
2025-04-01 10:36:47 +0100

    Designed fitness page

--- app/src/app/pages/main/settings/types/fitness-data/fitness-data.page.ts
+++ app/src/app/pages/main/settings/types/fitness-data/fitness-data.page.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonCard, IonCardContent,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonInput,
+  IonItem, IonLabel, IonList, IonNote, IonRow, IonSelect, IonSelectOption,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-fitness-data',
   templateUrl: './fitness-data.page.html',
   styleUrls: ['./fitness-data.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCol, IonGrid, IonInput, IonItem, IonList, IonRow, IonCard, IonCardContent, IonLabel, IonNote, IonSelect, IonSelectOption]
 })
 export class FitnessDataPage implements OnInit {
 
