be8aa3b331773702a3d56dfcbe92a9a70876027e
McDonaghMichael <G00421944@atu.ie>
2025-04-01 21:20:23 +0100

    Added icons to add menu selector

--- app/src/app/pages/main/homepage/components/add-menu/add-menu.component.ts
+++ app/src/app/pages/main/homepage/components/add-menu/add-menu.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import {IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle} from "@ionic/angular/standalone";
 import {RouterLink} from "@angular/router";
+import {NgIcon, provideIcons} from "@ng-icons/core";
+import {
+  featherAlignJustify,
+  featherType
+} from "@ng-icons/feather-icons";
 
 @Component({
   selector: 'app-add-menu',
   templateUrl: './add-menu.component.html',
+  providers: [provideIcons({ featherType, featherAlignJustify })],
   styleUrls: ['./add-menu.component.scss'],
   imports: [
     IonButton,
     IonCard,
     IonCardContent,
-    IonCardHeader,
-    IonCardTitle,
-    RouterLink
+    RouterLink,
+    NgIcon
   ]
 })
 export class AddMenuComponent  implements OnInit {
