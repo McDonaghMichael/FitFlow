89430a1a8fad6a526444d05c369f58db68a81754
McDonaghMichael <G00421944@atu.ie>
2025-04-01 10:40:26 +0100

    Added water reminder to notifications.page.html

--- app/src/app/pages/main/settings/types/notifications/notifications.page.ts
+++ app/src/app/pages/main/settings/types/notifications/notifications.page.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {IonCol, IonContent, IonGrid, IonHeader, IonRow, IonTitle, IonToolbar} from '@ionic/angular/standalone';
+import {
+  IonCard,
+  IonCardContent,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader, IonInput, IonItem, IonLabel,
+  IonRow,
+  IonTitle, IonToggle,
+  IonToolbar
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.page.html',
   styleUrls: ['./notifications.page.scss'],
   standalone: true,
-    imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCol, IonGrid, IonRow]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCol, IonGrid, IonRow, IonCard, IonCardContent, IonInput, IonItem, IonLabel, IonToggle]
 })
 export class NotificationsPage implements OnInit {
 
