f2678b3b97a6d878fbf7ae6f3d3c9784d98bfddb
McDonaghMichael <G00421944@atu.ie>
2025-04-02 12:32:39 +0100

    Created backend

---
+++ backend/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/rs/cors"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	envError := godotenv.Load("./.env")
+
+	if envError != nil {
+		log.Fatalln("[FAILURE] Error loading .env file")
+	} else {
+		log.Default().Println("[SUCCESS] Loaded .env file")
+	}
+
+	uri := os.Getenv("MONGO_DATABASE_URL")
+
+	_, err := mongo.Connect(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Print("[FAILURE]", err)
+	} else {
+		log.Default().Println("[SUCCESS] Connected to MongoDB!")
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", handleHome)
+
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:3000"},
+		AllowedMethods: []string{"GET", "POST"},
+		AllowedHeaders: []string{"Content-Type"},
+	}).Handler(r)
+
+	http.ListenAndServe(":8080", corsHandler)
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
