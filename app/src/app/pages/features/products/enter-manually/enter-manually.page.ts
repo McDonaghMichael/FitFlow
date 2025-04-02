cd1e2e2727840952d8ae2978ff8db882c1bd61e3
McDonaghMichael <G00421944@atu.ie>
2025-04-02 13:21:57 +0100

    Products are now taken from the API directly

--- app/src/app/pages/features/products/enter-manually/enter-manually.page.ts
+++ app/src/app/pages/features/products/enter-manually/enter-manually.page.ts
@@ -13,6 +13,7 @@ import {
 } from '@ionic/angular/standalone';
 import { TabMenuComponent } from "../../../../components/tab-menu/tab-menu.component";
 import {Router, RouterLink} from "@angular/router";
+import {ProductService} from "../../../../services/product.service";
 
 @Component({
   selector: 'app-enter-manually',
@@ -26,21 +27,17 @@ export class EnterManuallyPage implements OnInit {
   search: string = '';
   selectedProduct: number = 0;
 
-  foodArray = [
-    { id: "aaa", name: 'Apple', brand: 'Tesco', calories: 95, carbs: 25, protein: 0.5, fibre: 4, rating: 4.5, barcodeNumber: 10800 },
-    { id: "aba", name: 'Banana', brand: 'Lidl', calories: 105, carbs: 27, protein: 1.3, fibre: 3.1, rating: 4.7, barcodeNumber: 1400 },
-    { id: "aaea", name: 'Orange Juice', brand: 'Aldi', calories: 120, carbs: 30, protein: 2, fibre: 0.5, rating: 4.2, barcodeNumber: 143000 },
-    { id: "aa43a", name: 'Granola Bar', brand: 'Dunnes', calories: 200, carbs: 30, protein: 4, fibre: 5, rating: 4.0, barcodeNumber: 1000 },
-    { id: "agaa", name: 'Apple Pie', brand: 'Lidl', calories: 250, carbs: 35, protein: 3, fibre: 3, rating: 3.8, barcodeNumber: 100230 },
-  ];
-
-
+  foodArray: any = [];
   filteredFoodArray: any = [];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private productService: ProductService) { }
 
   ngOnInit() {
-    this.filteredFoodArray = [...this.foodArray];
+    this.productService.getProducts().subscribe({
+      next: (data) => this.foodArray = data,
+      error: (err) => console.error('Error fetching products:', err)
+    });
+
     const prod = localStorage.getItem('product');
     if (prod) {
       this.selectedProduct = Number(prod);
@@ -49,11 +46,12 @@ export class EnterManuallyPage implements OnInit {
 
   updateResults(event: any): void {
     const value = event.target.value.toLowerCase();
-    this.filteredFoodArray = this.foodArray.filter(food =>
+    this.filteredFoodArray = this.foodArray.filter((food: { name: string; brand: string; barcodeNumber: any; }) =>
       food.name.toLowerCase().includes(value) ||
       food.brand.toLowerCase().includes(value) ||
       food.barcodeNumber == value
     );
+
   }
 
   selectProduct(product: number): void {
