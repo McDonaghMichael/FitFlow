a4a85c6d9e629f2da978e76d2a8dae5ae907f5a8
McDonaghMichael <G00421944@atu.ie>
2025-04-02 10:05:46 +0100

    Added star rating system

--- app/src/app/pages/features/products/product-information/product-information.page.ts
+++ app/src/app/pages/features/products/product-information/product-information.page.ts
@@ -2,19 +2,20 @@ import {AfterViewInit, Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
+  IonButton,
   IonCard,
   IonCardContent,
   IonCol,
   IonContent, IonGrid,
-  IonHeader,
-  IonIcon,
-  IonRow,
-  IonTitle,
-  IonToolbar
+  IonRow
 } from '@ionic/angular/standalone';
 import {TabMenuComponent} from "../../../../components/tab-menu/tab-menu.component";
 import Chart from 'chart.js/auto';
 import {NotificationComponent} from "../../../../components/notification/notification.component";
+import {NgIcon, provideIcons} from "@ng-icons/core";
+import {
+  featherStar
+} from "@ng-icons/feather-icons";
 
 
 @Component({
@@ -22,7 +23,8 @@ import {NotificationComponent} from "../../../../components/notification/notific
   templateUrl: './product-information.page.html',
   styleUrls: ['./product-information.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonIcon, TabMenuComponent, IonRow, IonCol, IonCard, IonCardContent, IonGrid, NotificationComponent]
+  providers: [provideIcons({ featherStar })],
+  imports: [IonContent, CommonModule, FormsModule, TabMenuComponent, IonRow, IonCol, IonCard, IonCardContent, IonGrid, NotificationComponent, IonButton, NgIcon]
 })
 export class ProductInformationPage implements OnInit, AfterViewInit {
 
@@ -37,19 +39,24 @@ export class ProductInformationPage implements OnInit, AfterViewInit {
   protein: number = 0;
   satisfaction: number = 0;
 
+  ratingArray: number[] = [];
+
 
   foodArray = [
-    { id: "aaa", name: 'Apple', brand: 'Tesco', calories: 95, carbs: 10, protein: 50, fibre: 40, rating: 4.5, barcodeNumber: 10800 },
-    { id: "aba", name: 'Banana', brand: 'Lidl', calories: 105, carbs: 27, protein: 10.3, fibre: 3.1, rating: 4.7, barcodeNumber: 1400 },
-    { id: "aaea", name: 'Orange Juice', brand: 'Aldi', calories: 120, carbs: 30, protein: 2, fibre: 0.5, rating: 4.2, barcodeNumber: 143000 },
-    { id: "aa43a", name: 'Granola Bar', brand: 'Dunnes', calories: 200, carbs: 30, protein: 4, fibre: 5, rating: 4.0, barcodeNumber: 1000 },
-    { id: "agaa", name: 'Apple Pie', brand: 'Lidl', calories: 250, carbs: 35, protein: 3, fibre: 3, rating: 3.8, barcodeNumber: 100230 },
+    { id: "aaa", name: 'Apple', brand: 'Tesco', calories: 95, carbs: 10, protein: 50, fibre: 40, rating: 3, barcodeNumber: 10800 },
+    { id: "aba", name: 'Banana', brand: 'Lidl', calories: 105, carbs: 27, protein: 10.3, fibre: 3.1, rating: 4, barcodeNumber: 1400 },
+    { id: "aaea", name: 'Orange Juice', brand: 'Aldi', calories: 120, carbs: 30, protein: 2, fibre: 0.5, rating: 2, barcodeNumber: 143000 },
+    { id: "aa43a", name: 'Granola Bar', brand: 'Dunnes', calories: 200, carbs: 30, protein: 4, fibre: 5, rating: 4, barcodeNumber: 1000 },
+    { id: "agaa", name: 'Apple Pie', brand: 'Lidl', calories: 250, carbs: 35, protein: 3, fibre: 3, rating: 5, barcodeNumber: 100230 },
   ];
 
   constructor() { }
 
   ngOnInit() {
     this.updateProduct();
+    for (let i = 0; i < this.rating; i++) {
+      this.ratingArray.push(i);
+    }
   }
 
   ngAfterViewInit() {
