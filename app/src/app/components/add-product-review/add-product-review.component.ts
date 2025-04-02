2611816d97fbb76e205f7cb5175d8be216e2d1e7
McDonaghMichael <G00421944@atu.ie>
2025-04-02 10:37:36 +0100

    Add product review menu now can be dynamically shown/unshown and displasy the correct data

--- app/src/app/components/add-product-review/add-product-review.component.ts
+++ app/src/app/components/add-product-review/add-product-review.component.ts
@@ -1,14 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonSelect, IonSelectOption,
+  IonTextarea
+} from "@ionic/angular/standalone";
+import {NgIcon, provideIcons} from "@ng-icons/core";
+import {RouterLink} from "@angular/router";
+import {featherAlignJustify, featherType, featherX} from "@ng-icons/feather-icons";
+import {FormsModule} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-add-product-review',
   templateUrl: './add-product-review.component.html',
+  providers: [provideIcons({ featherX })],
   styleUrls: ['./add-product-review.component.scss'],
+  imports: [
+    IonButton,
+    IonCard,
+    IonCardContent,
+    NgIcon,
+    IonTextarea,
+    IonSelect,
+    IonSelectOption,
+    FormsModule,
+    NgIf
+  ]
 })
-export class AddProductReviewComponent  implements OnInit {
+export class AddProductReviewComponent implements OnInit {
+
+  @Input() showMenu = true;
+  review: string = "";
+  rating: number = 0;
 
   constructor() { }
 
   ngOnInit() {}
+
+  addProductReview() : void {
+    console.log(this.review);
+    console.log(this.rating);
+  }
+
+  closeMenu() : void {
+    this.showMenu = false;
+  }
 
 }
