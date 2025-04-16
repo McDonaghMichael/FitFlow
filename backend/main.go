c0bed7b70e7fb5cf9876725177134e968363fa3e
McDonaghMichael <G00421944@atu.ie>
2025-04-16 16:38:44 +0100

    Updated backend url

--- backend/main.go
+++ backend/main.go
@@ -49,16 +49,13 @@ func main() {
 	r.HandleFunc("/review/rating/{barcode}", routes.FetchRating(client)).Methods("GET")
 
 	corsHandler := cors.New(cors.Options{
-		AllowedOrigins:   []string{"https://localhost:4200", "http://185.81.166.93:4200", "https://fitlow.mcdonagh.xyz", "http://192.168.1.31:8080", "http://192.168.1.53", "http://192.168.56.1:4200", "http://192.168.1.31:4200"},
-		AllowedMethods:   []string{"GET", "POST"},
-		AllowedHeaders:   []string{"Content-Type"},
+		AllowedOrigins:   []string{"http://185.81.166.93:4200"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
 		AllowCredentials: true,
 	}).Handler(r)
 
 	err = http.ListenAndServe(":8181", corsHandler)
-	if err != nil {
-		log.Fatal("[FAILURE] HTTP Server Error:", err)
-	}
 }
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
